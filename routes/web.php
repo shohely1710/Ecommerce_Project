253e6373399e8710d22648ebdb75e4d898b2104e
shohely1710 <66716089+shohely1710@users.noreply.github.com>
2021-04-24 10:12:12 +0600

    Search Products Done

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
@@ -30,12 +31,14 @@ Route::get('/shop',ShopComponent::class);
 Route::get('/cart',CartComponent::class)->name('product.cart');
 Route::get('/checkout',CheckoutComponent::class);
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
-Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category')
+Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
+
+Route::get('/search',SearchComponent::class)->name('product.search');
 
 //Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //    return view('dashboard');
 //})->name('dashboard');
-;
+
 //For User or Customer
 
 Route::middleware('auth:sanctum', 'verified')->group(function(){
